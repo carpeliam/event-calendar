1eadd691adf2ad3189a236c7e2e7e5b0b7b3a736
Liam Morley <lmorley@pivotal.io>
2017-05-08 00:03:51 -0400

    WIP DO NOT MERGE OTHERWISE ALL OF YOUR LOVERS WILL BE CATS
    - Spiked on Google Calendar API w/OAuth2
    - console.logged link redirects back to /authorize - will want to store token
    - see CF for client id and secret (redirectUrl can be http://localhost:3000/authorize in dev)
    - helpful URLs:
      * https://developers.google.com/google-apps/calendar/quickstart/nodejs
      * https://github.com/google/google-api-nodejs-client/#authorizing-and-authenticating
      * https://github.com/google/google-auth-library-nodejs

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ var app = express();
 
 var isProd = process.env.NODE_ENV === 'production';
 
+// Homepage
 if (isProd) {
   app.use(express.static(compiler.outputPath));
   app.get('/', function (req, res) {
@@ -30,6 +31,61 @@ if (isProd) {
     });
   });
 }
+
+
+var clientId = process.env.GOOGLE_CALENDAR_API_CLIENT_ID;
+var clientSecret = process.env.GOOGLE_CALENDAR_API_CLIENT_SECRET;
+var redirectUrl = process.env.GOOGLE_CALENDAR_API_REDIRECT_URL;
+
+var google = require('googleapis');
+var OAuth2 = google.auth.OAuth2;
+var oauth2Client = new OAuth2(clientId, clientSecret, redirectUrl);
+var googleOAuth2Url = oauth2Client.generateAuthUrl({ scope: 'https://www.googleapis.com/auth/calendar' });
+console.log(googleOAuth2Url);
+
+// /authorize
+app.get('/authorize', function(req, res) {
+  var code = req.query.code;
+
+  retrieveItems(code, function(err, items) {
+    if (err) {
+      console.log('The API returned an error: ' + err);
+      res.send(err);
+    }
+    console.log('Upcoming 10 events:');
+    var arr = [];
+    for (var i = 0; i < items.length; i++) {
+      var event = items[i];
+      var start = event.start.dateTime || event.start.date;
+      console.log('%s - %s', start, event.summary);
+      arr[i] = {start: start, summary: event.summary};
+    }
+
+    res.send(arr);
+  });
+});
+
+function retrieveItems(code, callback) {
+  oauth2Client.getToken(code, function(err, token) {
+    if (err) {
+      console.log('Error while trying to retrieve access token', err);
+      return;
+    }
+    oauth2Client.credentials = token;
+    // storeToken(token);
+    var calendar = google.calendar('v3');
+    calendar.events.list({
+      auth: oauth2Client,
+      calendarId: 'primary',
+      timeMin: (new Date()).toISOString(),
+      maxResults: 10,
+      singleEvents: true,
+      orderBy: 'startTime'
+    }, function(err, response) { callback(err, response.items); });
+  });
+}
+
+
 
 var port = process.env.PORT || 3000;
 
